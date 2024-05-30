49557caf53a0a34de8d4f1dee9ebe4ada7185b07
4lverto <kesolesito@gmail.com>
2024-05-30 12:17:36 +0200

    API de whatsapp añadida

--- atencion_cliente.py
+++ atencion_cliente.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 atencion_cliente = Blueprint('atencion_cliente', __name__)
 
 @atencion_cliente.route('/atencion_cliente')
 def atencion_cliente_page():
     return render_template('atencion_cliente.html')
+
+@atencion_cliente.route('/enviar_consulta', methods=['POST'])
+def enviar_consulta():
+    nombre = request.form['nombre']
+    email = request.form['email']
+    mensaje = request.form['mensaje']
+    # Aquí puedes procesar y almacenar los datos si es necesario
+
+    # Construir la URL de WhatsApp
+    whatsapp_url = f"https://wa.link/5drjts?text={nombre}%20{email}%20{mensaje}"
+    return redirect(whatsapp_url)
